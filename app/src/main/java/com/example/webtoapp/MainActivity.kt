fce1f1e7131c75df9787819096af7e520a55202a
bhavyansh <bhavyansh2018@gmail.com>
2024-04-01 15:45:26 +0530

    Finial Commit

--- app/src/main/java/com/example/webtoapp/MainActivity.kt
+++ app/src/main/java/com/example/webtoapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.webtoapp
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.webtoapp.ui.theme.WebToAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +14,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WebToAppTheme {
-
+                WebScreen()
             }
         }
     }
+}
+
+@Composable
+fun WebScreen() {
+
+    AndroidView(factory = { context ->
+        return@AndroidView WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+    },
+        update = {
+            it.loadUrl("https://www.niccolomiranda.com")
+        }
+    )
+
 }
